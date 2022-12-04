f63eb2fc8403f87bb9361f3587faf18742780b72
Stephen Moore <stephen@delfick.com>
2022-12-04 16:48:02 +1100

    tooling: converting to newer pattern

---
+++ tools/devtools.py
@@ -0,0 +1,71 @@
+from pathlib import Path
+import typing as tp
+import inspect
+import shlex
+import sys
+import os
+
+here = Path(__file__).parent
+
+
+class Command(tp.Protocol):
+    __is_command__: bool
+
+    def __call__(self, bin_dir: Path, args: list[str]) -> None:
+        ...
+
+
+def command(func: tp.Callable) -> tp.Callable:
+    tp.cast(Command, func).__is_command__ = True
+    return func
+
+
+def run(*args: str | Path, _env: None | dict[str, str] = None) -> None:
+    cmd = " ".join(shlex.quote(str(part)) for part in args)
+    print(f"Running '{cmd}'")
+    ret = os.system(cmd)
+    if ret != 0:
+        sys.exit(ret)
+
+
+class App:
+    commands: dict[str, Command]
+
+    def __init__(self):
+        self.commands = {}
+
+        compare = inspect.signature(type("C", (Command,), {})().__call__)
+
+        for name in dir(self):
+            val = getattr(self, name)
+            if getattr(val, "__is_command__", False):
+                assert (
+                    inspect.signature(val) == compare
+                ), f"Expected '{name}' to have correct signature, have {inspect.signature(val)} instead of {compare}"
+                self.commands[name] = val
+
+    def __call__(self, args: list[str]) -> None:
+        bin_dir = Path(sys.executable).parent
+
+        if args and args[0] in self.commands:
+            os.chdir(here.parent)
+            self.commands[args[0]](bin_dir, args[1:])
+            return
+
+        sys.exit(f"Unknown command:\nAvailable: {sorted(self.commands)}\nWanted: {args}")
+
+    @command
+    def format(self, bin_dir: Path, args: list[str]) -> None:
+        if not args:
+            args = [".", *args]
+        run(bin_dir / "black", *args)
+
+    @command
+    def lint(self, bin_dir: Path, args: list[str]) -> None:
+        run(bin_dir / "pylama", *args)
+
+
+app = App()
+
+if __name__ == "__main__":
+    app(sys.argv[1:])
